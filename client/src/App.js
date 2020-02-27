1af901d52e3bf18351e568464567a77083654f0f
Alix de Ribet <alixderibet@gmail.com>
2020-02-27 17:51:06 +0100

    front set up

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="home">
+    APPZZ
+    </div>
+  );
+}
+
+export default App;
