59ddf0080177673fde55267cfe0b1f795481dfd0
Alix de Ribet <alixderibet@gmail.com>
2020-02-28 17:11:45 +0100

    ads displayed

--- client/src/views/AdsDisplayed.jsx
+++ client/src/views/AdsDisplayed.jsx
@@ -10,18 +10,21 @@ export default class Ads extends Component {
   componentDidMount() {
     APIHandler.get("/ads")
     .then(apiRes => {
-      this.setState({ ads: apiRes.data.ads })
+      console.log(apiRes.data);
+      this.setState({ ads: apiRes.data });
+    })
     .catch(err => console.error(err));
-    });
   }
 
   render() {
+    console.log(this.state.ads);
     return (
+
       <React.Fragment>
 
         <h1 className="title">Toutes les annonces</h1>
 
-        {Boolean(this.state.ads.length) ? this.state.ads.map((ad, i) => (
+        {Boolean((this.state.ads).length) ? this.state.ads.map((ad, i) => (
           <PreviewAd data={ad}/>
         )) : <p>Aucune annonce...</p>}
 
