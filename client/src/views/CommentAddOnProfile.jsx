155fd9ea7c2d1615645bacd796925cf29e5f6c42
Pauline Morsli <pauline.morsli@gmail.com>
2020-03-02 17:21:13 +0100

    add a comment on user profile

---
+++ client/src/views/CommentAddOnProfile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddComment from "../components/comments/AddComment";
+
+function CommentAddOnProfile() {
+    return (
+        <div>
+            <AddComment />
+        </div>
+    )
+}
+
+export default CommentAddOnProfile
