233162e1e376d67f5c9de4352a25ec967da0e4cd
Pauline Morsli <pauline.morsli@gmail.com>
2020-03-03 12:33:12 +0100

    comment on user profile

--- client/src/components/comments/OneComment.jsx
+++ client/src/components/comments/OneComment.jsx
@@ -5,6 +5,7 @@ export default function OneComment({comment}) {
     return (
         <div className='comment'>
             <p className="ad-details">{comment.text}</p>
+            {/* <p className="ad-details">{comment.rate}</p> */}
         </div>
     )
 }
--- client/src/views/UserPublicProfile.jsx
+++ client/src/views/UserPublicProfile.jsx
@@ -1,29 +1,33 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import apiHandler from "../api/APIHandler";
 import UserInfos from "../components/user/UserInfos";
 import AddComment from "../components/comments/AddComment";
 import DisplayComments from "../components/comments/DisplayComments";
 import DisplayAds from "../components/ad/DisplayAds";
- 
- export default function UserPublicProfile({match}) {
-     const [userInfos, setUserInfos] = useState (null)
-     useEffect(()=>{
-         apiHandler.get(`/users/${match.params.id}`)
-         .then(apiRes => {
-             console.log(apiRes.data)
-             setUserInfos(apiRes.data)})
-         .catch(apiErr => console.error(apiErr))
-     }, [])
-     return (
-         <div className='profile-page'>
-            <div className='profile-aside'>
-             <UserInfos userInfos={userInfos}/>
-             <DisplayAds ads={userInfos && userInfos.ads}/>
-             </div>
-             <DisplayComments/>
-             <AddComment userId={match.params.id}/>
-         </div>
-     )
- }
- 
+
+export default function UserPublicProfile({ match }) {
+  const [userInfos, setUserInfos] = useState(null);
+  useEffect(() => {
+    apiHandler
+      .get(`/users/${match.params.id}`)
+      .then(apiRes => {
+        console.log(apiRes.data);
+        setUserInfos(apiRes.data);
+      })
+      .catch(apiErr => console.error(apiErr));
+  }, []);
+
+  console.log(userInfos);
+
+  return (
+    <div className="profile-page">
+      <div className="profile-aside">
+        <UserInfos userInfos={userInfos} />
+        <DisplayAds ads={userInfos && userInfos.ads} />
+      </div>
+      {userInfos && <DisplayComments comments={userInfos.comments}/>}
+      <AddComment userId={match.params.id} />
+    </div>
+  );
+}
