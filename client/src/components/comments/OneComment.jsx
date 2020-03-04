c9a15300a1a57d3ddcea31547055b3ea88308640
Pauline Morsli <pauline.morsli@gmail.com>
2020-03-04 12:41:31 +0100

    comment of comments

--- client/src/components/comments/OneComment.jsx
+++ client/src/components/comments/OneComment.jsx
@@ -4,6 +4,7 @@ export default function OneComment({ comment, clbk }) {
   return (
     <div className="comment">
       <p className="ad-details">{comment.text}</p>
+      {comment.response.map((r) => <p key={r._id}>{r.text}</p> )}
       <button
         onClick={e => clbk(e, comment._id)}
         className="button is-primary is-rounded"
